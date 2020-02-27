af497d14cccfc3669108fc8d35af980f2e21a9e2
Jacob Myers <jrmyers5@ncsu.edu>
2020-02-27 13:23:04 -0500

    Add base python script

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Hearddit
+An auditory version of the popular news site, Reddit.com
+
+## State of the Project:
+Functionality:
+- [ ] Retrieve a single post
+    - [x] Get the subreddit of the post and read it out
+    - [x] Get the title of that post and read it out
+        - [ ] Wait a few seconds for some interaction, if interaction then read the description
+            - [ ] Wait a few seconds for some interaction, if interaction then read the top level comment
+                - [ ] Repeat
+            - [ ] Provide ability to get out of the comment section and go to the next post
+- [ ] Figure out how to support infinite number of posts
+
+
+
+Bugs:
+- [x] Since it gets the name of the subreddit as a single word, pronunciation suffers
+    - [x] Once we figure out which subreddits we will support, could have a dictionary containing the plain english translation of all of them?
+
+- [ ] Detect urls in comment and skip them, if not the voice just reads the whole thing
+- [ ] Stop pronouncing the markdown characters (like bold being “asterisk asterisk”)
+
+- [ ] Right now it reads all from one subreddit before switching, want the ability to read 1 from a subreddit then move to the next one
+    - [ ] Remove the one that was just read then move on
+    - [ ] Put the reading in a while loop, once it hits the end of the “posts” list then reset the index to 
+
+
+## Stretch Goals:
+- [ ] Voice-related:
+    - [ ] Parse the comment/description and if they are identified as a female then set the gender of the voice 
+- [ ] Ability to save that post for later (would only save on the app, not on their Reddit account)
+- [ ] Randomize to a text based subreddit (not specified, using the collection below or a different collection)
+- [ ] Ability to upvote, downvote, clear_vote(), guild, save
+- [ ] Identifying the link domain name
---
+++ src/Hearddit.py
@@ -0,0 +1,123 @@
+import praw
+import pyttsx3
+import random
+import re
+
+# Houses all of the posts retrieved by the bot
+# ['subname': [list of posts], 'subname2': [list of posts]]
+allPosts = {}
+
+# The (surprisingly few) voices that speak english
+availableVoices = [
+	'com.apple.speech.synthesis.voice.karen',
+	'com.apple.speech.synthesis.voice.Alex',
+	'com.apple.speech.synthesis.voice.samantha.premium'
+]
+
+# Contains all of the subreddits supported so far, with their
+# plain English pronounciations
+subs = {
+	'askreddit' : 'ask reddit'
+	# 'cscareerquestions' : 'c s career questions'
+	# 'relationship_advice' : 'relationship advice',
+	# 'legaladvice' : 'legal advice'
+}
+
+# Create the Reddit instance and log in
+reddit = praw.Reddit(client_id='MTJGEDobi5ZS6A',
+	client_secret="C7gQm0rTtWargf6oWyLkY-4vryU", 
+	password='butts123', user_agent='buckin_boolin_bot',
+	username='boolin_bot')
+
+# Create the speech instance
+# Library docs: https://pyttsx3.readthedocs.io/en/latest/
+# Github: https://github.com/nateshmbhat/pyttsx3
+engine = pyttsx3.init()
+engine.setProperty('rate', 180) # raise this to increase the rate of speaking
+voices = engine.getProperty('voices')
+
+# def input():
+# 	try:
+# 		print("You have 5 seconds to respond before moving on")
+# 		foo = raw_input()
+# 		return foo
+# 	except:
+# 		# timeout
+# 		return
+
+def getPosts(subName, numPosts):
+	""" 
+		Gets the specified number of posts 
+		from the specified subreddit and appends
+		them to the allPosts list.
+	"""
+	subreddit = reddit.subreddit(subName)
+	for post in subreddit.hot(limit=numPosts):
+		if (subName in allPosts.keys()):
+			# already exists, append to the existing list
+			allPosts[subName].append(post)
+		else:
+			temp = [post]
+			allPosts[subName] = temp
+
+
+# Retrieve all of the posts from the specified sub
+for sub in subs.keys():
+	getPosts(sub, 2)
+print(str(allPosts))
+
+# allPosts architecture: ['subname': ['post1', 'post2'], 'subname2': ['post1', 'post2']]
+for sub in allPosts:
+	for post in allPosts[sub]:
+		try:
+			# Change voices (randomize)
+			randomVoice = random.randint(0, len(availableVoices) - 1)
+			engine.setProperty('voice', availableVoices[1])
+			engine.say("Sub Reddit: ..." + subs.get(sub) + ",")
+			engine.say("Title: ...")
+			engine.say(str(post.title) + "...")
+
+			# Read the description if it exists
+			if (post.selftext != ""):
+				engine.say("... Description: ...")
+				engine.say(str(post.selftext))
+
+			# Moves onto the next post if the current one has no comments
+			if (post.comments.__len__() < 1): continue
+			
+			# Gets the first 5 top level comments
+			if (post.comments.__len__() >= 5):
+				try:
+					count = 0
+					engine.say("Comments: ...")
+					for comment in post.comments.list():
+						# check to see if there is a url
+						# if so
+						#   get the url
+						#   get the indexOf the url using the url
+						#   if the index is anything other than 0
+						#     grab the substring from 0 to the beginning of the url
+						#     "Included Hyperlink"
+						#     grab the substring from the end of the url to the end of the comment
+						#   else if the index is 0
+						#     "Included Hyperlink"
+						#     grab the substring from the end of the url to the end of the comment
+
+						randomVoice = random.randint(0, len(availableVoices) - 1)
+						engine.setProperty('voice', availableVoices[randomVoice])
+						engine.say(str(comment.body))
+						engine.runAndWait()
+						count = count + 1
+						if count == 5:
+							break
+				except AttributeError:
+					continue
+
+		except:
+			continue
+engine.setProperty('voice', availableVoices[1])
+engine.say("... End of Posts ..." + subs.get(sub) + ",")
+engine.runAndWait()
+
+
+
